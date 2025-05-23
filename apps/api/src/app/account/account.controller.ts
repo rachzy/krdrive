57f403c3a096e6f94699a458cfaf136d87905317
rachzy <henriqueferreira.fox@gmail.com>
2025-05-23 02:52:40 -0300

    refactor: comment out user registration endpoint in AccountController

--- apps/api/src/app/account/account.controller.ts
+++ apps/api/src/app/account/account.controller.ts
@@ -7,22 +7,20 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 import { AccountService } from './account.service';
-import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './schemas/user.schema';
-import { GetUserDto } from './dto/get-user.dto';
 
 @Controller('account')
 export class AccountController {
   constructor(private readonly accountService: AccountService) {}
 
-  @Post('register')
-  async register(
-    @Body() createUserDto: CreateUserDto
-  ): Promise<Omit<User, 'password'>> {
-    const user = await this.accountService.createUser(createUserDto);
-    const { password, ...result } = user.toObject();
-    return result;
-  }
+  // @Post('register')
+  // async register(
+  //   @Body() createUserDto: CreateUserDto
+  // ): Promise<Omit<User, 'password'>> {
+  //   const user = await this.accountService.createUser(createUserDto);
+  //   const { password, ...result } = user.toObject();
+  //   return result;
+  // }
 
   @Get('get-user/:id')
   async getUser(@Param('id') id: string): Promise<Omit<User, 'password'>> {
